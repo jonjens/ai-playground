d515415c65ba45a081a02b49a3574e2b7eb74b8c
Jens Jonassen <jonassenjens@gmail.com>
2023-11-19 23:40:41 +0100

    Added OpenAi Dall-e 2 image variations

--- src/main/java/com/jens/aiplayground/controller/OpenAiController.java
+++ src/main/java/com/jens/aiplayground/controller/OpenAiController.java
@@ -1,57 +1,55 @@
 package com.jens.aiplayground.controller;
 
 import com.jens.aiplayground.service.EnvService;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @RestController
 public class OpenAiController {
 
     @Autowired
     EnvService envService;
 
-    private final String API_KEY = envService.getKey();
-    private static final String DALLE_API_ENDPOINT = "https://api.openai.com/v1/images/variations";
+    private static final String API_ENDPOINT = "https://api.openai.com/v1/images/variations";
     private static final String UPLOAD_DIR = "C:\\Users\\jonas\\uploadpath\\";
 
-    // ... (other parts of the controller)
+    @PostMapping("/dalle")
+    public ResponseEntity<byte[]> getDALLEResponse(MultipartFile file) {
+        String API_KEY = envService.getKey();
 
-    @PostMapping(value = "/generate", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.IMAGE_JPEG_VALUE)
-    public byte[] generateImageVariation(@RequestBody MultipartFile file) throws IOException {
-        if (file.isEmpty()) {
-            throw new IllegalArgumentException("Uploaded file is empty");
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost request = new HttpPost(API_ENDPOINT);
+        request.setHeader("Authorization", "Bearer " + API_KEY);
+
+        try {
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.addBinaryBody("image", file.getBytes(), ContentType.APPLICATION_OCTET_STREAM, file.getOriginalFilename());
+            builder.addTextBody("n", "1");
+            builder.addTextBody("size", "512x512");
+
+            HttpEntity multipart = builder.build();
+            request.setEntity(multipart);
+
+            HttpResponse response = httpClient.execute(request);
+
+            // Process the response and return it
+            return ResponseEntity.status(response.getStatusLine().getStatusCode())
+                    .body(response.getEntity().getContent().readAllBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build(); // Handle error response
         }
-
-        // Save uploaded file to a temporary location
-        String uploadedFileName = file.getOriginalFilename();
-        Path uploadPath = Paths.get(UPLOAD_DIR + uploadedFileName);
-        Files.copy(file.getInputStream(), uploadPath);
-
-        // Simulate calling DALL-E 2 API for image variation
-        byte[] variationImageData = callDALLE2API(uploadPath);
-
-        // Delete the temporary uploaded file
-        Files.deleteIfExists(uploadPath);
-
-        return variationImageData;
     }
-
-    // Simulated method for calling DALL-E 2 API (replace with actual API call)
-    private byte[] callDALLE2API(Path imagePath) throws IOException {
-        // Here, you'd actually make an API call to DALL-E 2 using the image at 'imagePath'
-        // Perform necessary processing and return the variation image data
-
-        // This is a placeholder to read and return the image data (not calling DALL-E 2)
-        return Files.readAllBytes(imagePath);
-    }
-
 }
--- src/test/java/com/jens/aiplayground/AiPlaygroundApplicationTests.java
+++ src/test/java/com/jens/aiplayground/AiPlaygroundApplicationTests.java
@@ -1,13 +1,24 @@
 package com.jens.aiplayground;
 
+import com.jens.aiplayground.service.EnvService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class AiPlaygroundApplicationTests {
 
+	@Autowired
+	EnvService envService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testKey(){
+		System.out.println(envService.getKey());
 	}
 
 }
